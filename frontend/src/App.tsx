c955bab1afd83e26c75b4efeaf653d315b7a90f7
Yair Levi <yairlevi2001@gmail.com>
2023-07-06 13:40:24 +0300

    Minor changes to schedule summary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,63 +1,21 @@
 import './input.css'
 import { Sidebar } from "@/components/Sidebar";
 import Calendar from "@/components/Calendar";
+import { Summary } from "@/components/calendar/Summary";
 
-function getHourOfDay(number: number, offset = 7) {
-  const hour = (number + offset) % 24
-  return `${hour}:00`
-  // const time = hour < 12 ? 'AM' : 'PM'
-  // if (hour === 0) {
-  //   return `12 ${time}`
-  // } else {
-  //   return `${hour % 12} ${time}`
-  // }
-  // return `${hour === 0 ? 12 : (hour % 12)} ${hour < 12 ? 'AM' : 'PM'}`;
-}
 
 export default function App() {
   return (
     <div className="flex h-screen bg-[#1a1c22] overflow-hidden">
       <Sidebar/>
-      <div className="flex w-full">
-        <div className="bg-[#1a1c22] w-full">
-          Hello
-        </div>
-        <div className="max-w-fit min-w-fit h-full bg-[#1a1c22] flex flex-col py-3">
-          <Calendar/>
-          <h1 className="px-3 text-gray-400 text-sm font-bold my-2">Schedule Summary</h1>
-          <div className="w-80 px-3 pb-4 overflow-y-auto">
-            {
-              [...Array(24).keys()].map((v, i) => {
-                function getRandomColor() {
-                  let letters = '0123456789ABCDEF';
-                  let color = '#';
-                  for (let i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * letters.length)];
-                  }
-                  return color;
-                }
-
-                const col = getRandomColor()
-                return (
-                  <div key={i} className="flex mb-2 gap-2">
-                    <div className="[&_*]:text-gray-500">
-                      <div className="w-10 text-right">
-                        <p className="font-semibold text-[0.8rem]">{getHourOfDay(i)}</p>
-                      </div>
-                    </div>
-                    <div className="w-10/12 self-end rounded-md py-1.5 px-2 flex items-center bg-[#0f0f11] gap-2">
-                      <div className={`border rounded-lg border-l-4 h-10`} style={{ borderColor: col }}/>
-                      <section>
-                        <p className="font-bold text-gray-300">Meeting with team</p>
-                      </section>
-                    </div>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
+      <div className="w-full">
+        Hello
       </div>
+      <div className="max-w-fit min-w-fit h-full bg-[#1a1c22] flex flex-col py-3">
+        <Calendar/>
+        <Summary/>
+      </div>
+
     </div>
   )
 }
---
+++ frontend/src/components/calendar/Summary.tsx
@@ -0,0 +1,43 @@
+function getHourOfDay(number: number, offset = 7) {
+  const hour = (number + offset) % 24
+  return `${hour}:00`
+}
+
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return color;
+}
+
+export function Summary() {
+  return (
+    <>
+      <h1 className="px-3 text-gray-400 text-xs font-bold my-2">Schedule Summary</h1>
+      <div className="w-80 px-3 pb-4 overflow-y-auto">
+        {
+          [...Array(24).keys()].map((v, i) => {
+            const col = getRandomColor()
+            return (
+              <div key={i} className="flex mb-2 gap-2">
+                <div className="[&_*]:text-gray-500">
+                  <div className="w-8 text-right">
+                    <p className="font-semibold text-xs">{getHourOfDay(i)}</p>
+                  </div>
+                </div>
+                <div className="w-11/12 self-end rounded-md py-1.5 px-2 flex items-center bg-[#0f0f11] gap-2">
+                  <div className={`border rounded-lg border-l-4 h-8`} style={{ borderColor: col }}/>
+                  <section>
+                    <p className="font-medium text-gray-300 text-sm">Meeting with team</p>
+                  </section>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
+  )
+}
