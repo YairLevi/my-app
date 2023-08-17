c19a9df6b1c0e8a6f95d51932a400fd8556b3462
Yair Levi <yairlevi2001@gmail.com>
2023-08-17 19:59:04 +0300

    Adding basic router functionality.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
 import { Sidebar } from "@/components/Sidebar"
 import { Calendar } from "@/components/calendar/Calendar"
 import { Summary } from "@/components/calendar/Summary"
-import { Grid } from "@/components/calendar/Grid"
 import React, { useState } from "react"
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 import './input.css'
 import { AddEventModal } from "@/components/calendar/AddEvent.modal";
-import {MonthlyCalendar } from '@/components/monthlyCalendar/MonthlyCalendar'
+import { MonthlyCalendar } from '@/components/monthlyCalendar/MonthlyCalendar'
+import { Route, Routes } from "react-router";
+import { Grid } from "@/components/calendar/Grid";
+import { Button } from "@/components/Button";
+import { useNavigate } from "react-router";
 
 
 export default function App() {
   const [open, setOpen] = useState(false)
   const onClose = () => setOpen(false)
+  const navigate = useNavigate()
 
   return (
     <div className="flex h-screen bg-[#1a1c22] overflow-hidden">
@@ -27,7 +31,15 @@ export default function App() {
           {/*    Add Event*/}
           {/*  </button>*/}
           {/*</div>*/}
-          <MonthlyCalendar/>
+          <div className="flex gap-2">
+            <Button onClick={() => navigate('/week')} color="#0f0f11">Go to week</Button>
+            <Button onClick={() => navigate('/month')} color="#0f0f11">Go to month</Button>
+          </div>
+          <Routes>
+            <Route path="/" element={<button className="text-white">Hello</button>}/>
+            <Route path="/week" element={<Grid/>}/>
+            <Route path="/month" element={<MonthlyCalendar/>}/>
+          </Routes>
         </div>
       </div>
       <div className="max-w-fit min-w-fit h-full bg-[#1a1c22] flex flex-col py-3">
@@ -39,5 +51,5 @@ export default function App() {
         onClose={onClose}
       />
     </div>
-  )
+)
 }
